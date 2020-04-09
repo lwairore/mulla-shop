0a0073669b57353041e7019679a8e70bd4d5cf37
lwairore <kwairore@gmail.com>
2020-04-09 23:15:15 +0300

    - Create product_list view function.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
 # Create your views here.
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = models.Category.objects.all()
+    products = models.Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(models.Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
