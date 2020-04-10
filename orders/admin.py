3daad9ec56a6101b14bd9a41ddbbb6b96ac4bdca
lwairore <kwairore@gmail.com>
2020-04-10 23:36:19 +0300

    - Add the order models to the administration site.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email',
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
