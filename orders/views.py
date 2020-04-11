ab9c84604541e132ca1869ff8a9151a5ce80adfc
lwairore <kwairore@gmail.com>
2020-04-12 00:24:56 +0300

    - On order_create view after successfully creating an order, we set the order ID  in the current session using the order_id session key. Then, we redirect the user to the `payment:process` URL.

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models, forms
 from cart.cart import Cart
 from .tasks import order_created
+from django.urls import reverse
 
 # Create your views here.
 
@@ -24,9 +25,10 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect
+            return redirect(reverse('payment:process'))
     else:
         form = forms.OrderCreateForm()
     return render(request,
