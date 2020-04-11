ae2f4d30ac8c6c1b1653417351eabba727666e51
lwairore <kwairore@gmail.com>
2020-04-12 01:05:36 +0300

    - Create admin_order_detail view thay gets the Order object which the given ID and render a template to display the order.

--- orders/views.py
+++ orders/views.py
@@ -3,8 +3,16 @@ from . import models, forms
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(models.Order, order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
 
 
 def order_create(request):
