cfc0c09427c4bc288d4f185bea2b6c5fe4b890b9
lwairore <kwairore@gmail.com>
2020-04-12 17:01:42 +0300

    - Add LOCALE_PATHS setting that specifies the directories where Django has to look for translation fileds.

--- myshop/settings.py
+++ myshop/settings.py
@@ -133,6 +133,10 @@ LANGUAGES = (
     ('es', 'Spanish'),
 )
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/')
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
