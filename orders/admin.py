6f230f6511e3eda1e7800833731e293c7c732f2f
lwairore <kwairore@gmail.com>
2020-04-12 13:41:59 +0300

    - Add order_pdf to the list_display attribute of the OrderAdmin class.

--- orders/admin.py
+++ orders/admin.py
@@ -57,7 +57,8 @@ order_pdf.short_description = 'Invoice'
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
+                    'address', 'postal_code', 'city', 'paid',
+                    'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
