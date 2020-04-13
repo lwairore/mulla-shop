4b75c600231ce4435f828b3fee645848ff361ff2
lwairore <kwairore@gmail.com>
2020-04-13 20:01:42 +0300

    - Moify the `CategoryAdmin` and `ProductAdmin` classes to inherit from `TranslatableAdmin` instead of `ModelAdmin`. - Change prepopulated_fields attribute to get_prepopulated_fields().

--- shop/admin.py
+++ shop/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
 from . import models
+from parler.admin import TranslatableAdmin
 
 # Register your models here.
 @admin.register(models.Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 
 @admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 
 """
