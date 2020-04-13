a8878f07be9e49d2c38248ab691d6b4e77370e51
lwairore <kwairore@gmail.com>
2020-04-13 20:49:22 +0300

    - Remove parler conf. completely

--- shop/models.py
+++ shop/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 from django.urls import reverse
-from parler.models import TranslatableModel, TranslatedFields
+
+# Create your models here.
 
 
-class Category(TranslatableModel):
-    translations = TranslatedFields(
-        name=models.CharField(max_length=200,
-                              db_index=True),
-        slug=models.SlugField(max_length=200,
-                              db_index=True,
-                              unique=True)
-    )
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        # ordering = ('name',)
+        ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -21,34 +17,32 @@ class Category(TranslatableModel):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category',
-                       args=[self.slug])
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
-class Product(TranslatableModel):
-    translations = TranslatedFields(
-        name=models.CharField(max_length=200, db_index=True),
-        slug=models.SlugField(max_length=200, db_index=True),
-        description=models.TextField(blank=True)
-    )
-    category = models.ForeignKey(Category,
-                                 related_name='products',
-                                 on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
-                              blank=True)
-
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    # class Meta:
-    #    ordering = ('name',)
-    #    index_together = (('id', 'slug'),)
+    class Meta:
+        ordering = ('name',)
+        """
+             use the index_together meta option to specify an index for the id and slug fields together. 
+             We define this index because we plan to query products by both id and slug. 
+             Both fields are indexed together to improve performances for queries that utilize the two fields.
+        """
+        index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail',
-                       args=[self.id, self.slug])
+        return reverse('shop:product_detail', args=[self.id, self.slug])
