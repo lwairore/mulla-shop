d68a4c25023886aa1b7f9a6f048005affc171a76
lwairore <kwairore@gmail.com>
2020-04-14 12:00:49 +0300

    - Create Recommender class that will allow us to store product purchases and retrieve product sggestions for a given product or products. - Add `get_product_key()` method that recievesan ID of a Product object and builds the Redis key for the sorted set where related products are stored, which looks like `product:[:id]:purchased_with`. - Define `products_bought()` method that receives a list of Product objects that have been bought together (that is, belong to the same order).

---
+++ shop/recommender.py
@@ -0,0 +1,25 @@
+import redis
+from django.conf import settings
+from .models import Product
+
+# connect to redis
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
+
+
+class Recommender(object):
+
+    def get_product_key(self, id):
+        return 'product:{}:purchased_with'.format(id)
+
+    def products_bought(self, products):
+        product_ids = [p.id for p in products]
+        for product_id in product_ids:
+            for with_id in product_ids:
+                # get the other products bought with each product
+                if product_id != with_id:
+                    # increment score for product purchased together
+                    r.zincrby(self.get_product_key(product_id),
+                              with_id,
+                              amount=1)
